7e359b6a5486124acccd72cf34044ed0b042f415
George Castillo <gek1994@gmail.com>
2022-03-06 10:40:55 -0500

    primer commit

---
+++ src/main/java/com/bootcamp/reactive/retoblog/RetoblogApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.reactive.retoblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetoblogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootcamp/reactive/retoblog/RetoblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.reactive.retoblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetoblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
